38b21f48378991ae7df258e7ec141073d6c2482f
gmenez <gilles.menez@univ-cotedazur.fr>
2021-12-02 15:28:12 +0100

    urls version

--- node_lucioles_v4.js
+++ node_lucioles_v4.js
@@ -198,7 +198,7 @@ app.use(function(request, response, next) { //Pour eviter les problemes de CORS/
     
 // Route / => Le node renvoie la page HTML affichant les charts
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'public/ui_lucioles.html'));
+    res.sendFile(path.join(__dirname + '/public/ui_lucioles.html'));
 });
 
 
